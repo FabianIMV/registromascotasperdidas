301c949a4f53b7c68e547812a4129dba52e48a2a
FabianIMV <fabianignaciomv@gmail.com>
2024-11-17 19:33:42 -0300

    se corrige import de logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../logo.png';
 
 const Navbar = () => {
   return (
     <nav className="bg-indigo-600 p-4 fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-16">
       <Link to="/" className="flex items-center h-full">
-        <img src="/logo.png" alt="Logo" className="h-full" />
+        <img src={logo} alt="Logo" className="h-full" />
       </Link>
       <div>
         <Link to="/perfil" className="text-white mr-4">Perfil</Link>
